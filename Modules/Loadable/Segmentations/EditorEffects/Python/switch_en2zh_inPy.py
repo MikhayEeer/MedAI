32beaae88d38209803bc91e2e67c8e29dca5f723
Danzel M <814416829@qq.com>
2024-08-21 20:15:54 +0800

    Scripts: script for translate EditorEffects

--- Modules/Loadable/Segmentations/EditorEffects/Python/switch_en2zh_inPy.py
+++ Modules/Loadable/Segmentations/EditorEffects/Python/switch_en2zh_inPy.py
@@ -1,6 +1,7 @@
 import os
-import json
 import re
+import json
+import argparse
 
 def find_strings_in_python_files(directory, search_pattern, output_file):
     results = []
@@ -49,20 +50,68 @@ def find_strings_in_python_files(directory, search_pattern, output_file):
     with open(output_file, write_mode, encoding='utf-8') as json_file:
         json.dump(results, json_file, indent=4, ensure_ascii=False)
 
+def replace_strings_in_python_files(directory, translation_file, mode = 'en2zh'):
+    if not mode in ['en2zh', 'zh2en']:
+        raise ValueError("Invalid mode. Please choose 'en2zh' or 'zh2en'.")
+    with open(translation_file, 'r', encoding='utf-8') as f:
+        translations = json.load(f)
+
+    translated_strings = {}
+    for item in translations:
+        original_string = item['string']
+        translated_string = item['翻译']
+        if original_string and translated_string:
+            translated_strings[original_string] = translated_string
+    
+    reversed_translations = {value: key for key, value in translated_strings.items()}
+
+    if mode == 'en2zh' :
+        print("Translating from English to Chinese.")
+        dict = translated_strings
+    else:
+        print("Translating from Chinese to English.")
+        dict = reversed_translations
+
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.py'):
+                file_path = os.path.join(root, file)
+                with open(file_path, 'r', encoding='utf-8') as f:
+                    content = f.read()
+
+                new_content = content
+                for original, translated in dict.items():
+                    pattern = re.escape(original)
+                    new_content = re.sub(rf'_\("{pattern}"\)', rf'_{translated!r}', new_content)
+                    new_content = re.sub(rf'_\(\'{pattern}\'\)', rf'_{translated!r}', new_content)
+
+                with open(file_path, 'w', encoding='utf-8') as f:
+                    f.write(new_content)
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--extract', action='store_true', help='Extract strings from Python files')
+    parser.add_argument('--replace', action='store_true', help='Replace strings in Python files based on a translation file')
+    args = parser.parse_args()
+
     pwd = os.getcwd()
     print(f"The current working directory is: {pwd}")
 
     relative_path ="Modules\Loadable\Segmentations\EditorEffects\Python"
     switch_directory = 'SegmentEditorEffects'
     output_file = 'editoreffects_translate_strings.json'
+    translation_file = 'editoreffects_translate_strings.json'
 
-    output_file = os.path.join(pwd, relative_path, output_file)
     switch_directory = os.path.join(pwd, relative_path, switch_directory)
+    print(f" The switch directory is: {switch_directory}")
+    
+    if args.extract:
+        output_file = os.path.join(pwd, relative_path, output_file)
+        print(f" The output file is: {output_file}")
+        search_pattern = r'_\(["\']([^"\']+?)["\']\)'
+        find_strings_in_python_files(switch_directory, search_pattern, output_file)
 
-    print(f"The output file is: {output_file}")
-    print(f"The switch directory is: {switch_directory}")
-
-    search_pattern = r'_\(["\']([^"\']+?)["\']\)'
-
-    find_strings_in_python_files(switch_directory, search_pattern, output_file)
+    if args.replace:
+        translation_file = os.path.join(pwd, relative_path, translation_file)
+        print(f" The translation file is: {translation_file}")
+        replace_strings_in_python_files(switch_directory, translation_file)
