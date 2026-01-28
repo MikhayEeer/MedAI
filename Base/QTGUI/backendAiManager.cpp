55b29bc8894d75995a07e7596eba213a63ae0e72
linhuanfengQ <linhuanfeng@gziis.org>
2026-01-28 10:21:13 +0800

    隐藏下旧版ai,并fix提示

--- Base/QTGUI/backendAiManager.cpp
+++ Base/QTGUI/backendAiManager.cpp
@@ -182,17 +182,17 @@ void Backend_AI_Processing_manager::uploadFile() {
         return;
     }
 
-    QString promtText = "AI气道分割";
+    QString promtText = "AI气道分割处理完成";
 
     QString baseName = complete_fileName.left(suffix_index); // aa
     m_result_path = file_dir_path + "/气道分割" + baseName + ".nii.gz"; // H:/b/气道分割aa.nii.gz
     if(_choosed_model != AI_MODEL::AIRWAY) {
         m_result_path = file_dir_path + "/肺部血管分割" + baseName + ".nii.gz";
-        promtText="AI肺部血管分割";
+        promtText="AI肺部血管分割处理完成";
     }
     if (_choosed_model == AI_MODEL::VESSELV2) {
         m_result_path = file_dir_path + "/肺段分割" + baseName + ".nii.gz";
-        promtText="AI肺部肺段分割";
+        promtText="AI肺部肺段分割处理完成";
     }
     qDebug() << filePath;
 //    qDebug() << file_dir_path;
@@ -201,12 +201,11 @@ void Backend_AI_Processing_manager::uploadFile() {
 //    qDebug() << m_result_path;
 
     QProgressDialog* progress = new QProgressDialog(this);
-    progress = new QProgressDialog(this);
     progress->setWindowTitle(tr(promtText.toStdString().c_str()));
     progress->setLabelText(tr("火速处理中..."));
     progress->setCancelButton(nullptr);
     progress->setRange(0, 63); //设置范围
-    progress->setValue(1); // 假值....
+    progress->setValue(15); // 假值....
     progress->setWindowFlags(progress->windowFlags() & ~Qt::WindowCloseButtonHint);
     progress->show();
     progressMap[filePath] = progress;
